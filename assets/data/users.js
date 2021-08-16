7e7425d5fe0fef4284897a87c97646ff4bf4cfdb
nakshatrasinghh <nakshatradsu@gmail.com>
2021-08-16 18:16:08 +0530

    matching screen in progress

--- assets/data/users.js
+++ assets/data/users.js
@@ -8,21 +8,21 @@ export default [
     },
     // more data can be added here or JSON can be stored in a URL and called using an API
     {
-      id: '1',
+      id: '2',
       name: 'Nakshatra Singh',
       image:
         'https://nakshatraasinghh.vercel.app/images/pexels-marc-schulte-2779948.jpg',
       bio: 'Deep Learning Practitioner',
     },
     {
-      id: '1',
+      id: '3',
       name: 'Nakshatra Singh',
       image:
         'https://raw.githubusercontent.com/nakshatrasinghh/nakshatrasinghh/master/readme.jpg',
       bio: 'Deep Learning Practitioner',
     },
     {
-      id: '1',
+      id: '4',
       name: 'Nakshatra Singh',
       image:
         'https://raw.githubusercontent.com/nakshatrasinghh/nakshatrasinghh/master/readme.jpg',
--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
+import users from '../../assets/data/users';
 
 const MatchesScreen = () => {
     return (
         <SafeAreaView style={styles.root}>
             <View styles={styles.container}>
                 <Text style={styles.title}>Matches</Text>
+                <View style={styles.users}>
+                    {users.map(user => (
+                    <View style={styles.user} key={user.id}>
+                        <Image source={{uri: user.image}} style={styles.image}/>
+                    </View>
+                ))}
+                </View>
+                
             </View>
         </SafeAreaView>
     )
@@ -13,17 +22,34 @@ const MatchesScreen = () => {
 
 const styles = StyleSheet.create({
     container: {
-        padding: 10
+        padding: 10,
     },
     title: {
         fontWeight: 'bold',
-        fontSize: 20,
+        fontSize: 24,
         color: '#F63A6E'},
     root: {
         width: '100%',
         flex: 1,
         padding: 10,
-
+    },
+    user: {
+        width: 100,
+        height: 100,
+        margin: 10,
+        borderWidth: 2,
+        borderRadius: 50,
+        borderColor: '#F63A6E',
+        padding: 3,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 50,
+    },
+    users: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
     }
 });
 
