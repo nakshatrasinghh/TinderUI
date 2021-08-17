302db6f17559df37293470ed3c171fd1bbf53c81
nakshatrasinghh <nakshatradsu@gmail.com>
2021-08-17 15:56:38 +0530

    matching screen UI

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
 import 'react-native-gesture-handler';
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 const App = () => {
 
+  const color = '#b5b5b5'
+
   return (
-    <View style={styles.pageContainer}> 
-      <MatchesScreen />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.pageContainer}> 
+        <View style={styles.topNavigation}>
+          <Fontisto name="tinder" size={30} color={color} /> 
+          <MaterialCommunityIcons name="star-four-points" size={30} color={color} />
+          <Ionicons name="ios-chatbubbles" size={30} color={color} />
+          <FontAwesome name="user" size={30} color={color} /> 
+        </View>
+        <HomeScreen />
+      </View>
+    </SafeAreaView>
+    
   );
 };
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer:{
     justifyContent: 'center', 
     alignItems: 'center', 
     flex: 1,
-    // backgroundColor: 'red'
+  },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
   },
 });
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,11 @@ import Card from '../components/TinderCard';
 import users from '../../assets/data/users';
 import AnimatedStack from '../components/AnimatedStack';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+
 const HomeScreen = () => {
 
   onSwipeLeft = user => {
@@ -20,7 +25,25 @@ const HomeScreen = () => {
       onSwipeLeft={onSwipeLeft}
       onSwipeRight={onSwipeRight}
       /> 
+      <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={30} color="#FBD88B" />
+        </View>
+        <View style={styles.button}>
+          <Entypo name="cross" size={30} color="#F76C6B" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="star" size={30} color="#3AB4CC" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={30} color="#4FCC94" />
+        </View>
+        <View style={styles.button}>
+          <Ionicons name="flash" size={30} color="#A65CD2" />
+        </View>   
+      </View>
     </View>
+    
   );
 };
 
@@ -30,7 +53,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center', 
     flex: 1,
-    // backgroundColor: 'red'
+    backgroundColor: '#ededed'
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%', 
+    paddingBottom: 10,
+  },
+  button: {
+    height: 50,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
+
+
   },
 });
 
