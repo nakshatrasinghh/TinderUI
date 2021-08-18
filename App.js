461c99f96a19b2bb66aeaeb1ef75a172ca4008d4
nakshatrasinghh <nakshatradsu@gmail.com>
2021-08-18 13:26:41 +0530

    icons and navigations

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, SafeAreaView, Pressable } from 'react-native';
 import 'react-native-gesture-handler';
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
@@ -11,18 +11,27 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
 
+  const [activeScreen, setActiveScreen] = useState('HOME');
+
   const color = '#b5b5b5'
+
+  const activeColor = '#F76C6B'
 
   return (
     <SafeAreaView style={styles.root}>
-      <View style={styles.pageContainer}> 
+      <View style={styles.pageContainer}>
         <View style={styles.topNavigation}>
-          <Fontisto name="tinder" size={30} color={color} /> 
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto name="tinder" size={30} color={activeScreen == 'HOME' ? activeColor : color} />
+          </Pressable>
           <MaterialCommunityIcons name="star-four-points" size={30} color={color} />
-          <Ionicons name="ios-chatbubbles" size={30} color={color} />
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons name="ios-chatbubbles" size={30} color={activeScreen == 'CHAT' ? activeColor : color} />
+          </Pressable>
           <FontAwesome name="user" size={30} color={color} /> 
         </View>
-        <HomeScreen />
+        {activeScreen == 'HOME' && <HomeScreen />}
+        {activeScreen == 'CHAT' && <MatchesScreen />}
       </View>
     </SafeAreaView>
     
